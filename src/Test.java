c05d9fcb044da01bce3380dce5a72253c2a619cb
rudojsru <rudojsru@gmail.com>
2019-02-16 00:10:33 +0100

    k

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+
+    public static void main(String[] args) {
+        int a=5;
+        int c=5;
+
+       while(true){
+        if ( a == c){
+            System.out.println("Yes!!!");
+            break;
+        }else {
+            System.out.println("No!!!");
+            break;
+        }
+       }
+    }
+}
